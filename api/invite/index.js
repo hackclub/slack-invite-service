25d77ccb40fe549dcdaecc8af17ac74f992e640a
safinsingh <safin.singh@gmail.com>
2021-01-21 16:57:29 -0800

    some code suggestions/cleanup

--- api/invite/index.js
+++ api/invite/index.js
@@ -1,46 +1,42 @@
 import fetch from 'node-fetch'
 
-export default async (req, res) => {
+export default async function invite(req, res) {
   // params: key, email, name, fullmember, channel
 
-  const data = req.body
+  const {
+    body: { key, email, name, fullmember, channel }
+  } = req
 
-  if (!data.key) {
-    return res.status(403).json({ error: `You must include a key. It's called "Slack invite service key" in 1Password`})
+  if (!key) {
+    return res.status(403).json({
+      error:
+        'You must include a key. It\'s called "Slack invite service key" in 1Password'
+    })
   }
-  if (data.key && data.key !== process.env.KEY) {
-    return res.status(403).json({ error: `Incorrect Slack invite service key`})
+
+  if (key !== process.env.KEY) {
+    return res.status(403).json({ error: 'Incorrect Slack invite service key' })
   }
 
   if (req.method === 'POST') {
-    let params
-    if (data.fullmember) {
-      params = [
-        `email=${data.email}`,
-        `token=${process.env.SLACK_LEGACY_TOKEN}`,
-        `real_name=${data.name}`,
-        `restricted=false`,
-        `channels=C74HZS5A5,${data.channel}`,
-        `resend=true`
-      ].join('&')
-    } else {
-      params = [
-        `email=${data.email}`,
-        `token=${process.env.SLACK_LEGACY_TOKEN}`,
-        `real_name=${data.name}`,
-        `restricted=true`,
-        `channels=C74HZS5A5`, // #lobby
-        `resend=true`
-      ].join('&') 
-    }
+    const params = [
+      `email=${email}`,
+      `token=${process.env.SLACK_LEGACY_TOKEN}`,
+      `real_name=${name}`,
+      `restricted=${fullmember ? 'false' : 'true'}`,
+      `channels=C74HZS5A5${fullmember ? ',' + channel : ''}`,
+      `resend=true`
+    ].join('&')
 
     const url = `https://slack.com/api/users.admin.invite?${params}`
     await fetch(url, { method: 'POST' })
-      .then(r => r.json())
-      .then(r => console.log('Slack response', r))
+      .then((r) => r.json())
+      .then((r) => console.log('Slack response', r))
 
-    res.json({ status: 'success', message: `You've been invited to Slack!` })
+    res
+      .status(200)
+      .json({ status: 'success', message: "You've been invited to Slack!" })
   } else {
-    return res.status(405).json({ error: 'Method not allowed, use POST'})
+    return res.status(405).json({ error: 'Method not allowed, use POST' })
   }
 }
